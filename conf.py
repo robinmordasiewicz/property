32d2ec31eb04e8033dae1e2d435c8e41c8539ac2
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-01-29 14:57:43 -0500

    adding skel

--- conf.py
+++ conf.py
@@ -181,9 +181,9 @@ todo_include_todos = True
 # a list of builtin themes.
 
 html4_writer = True
-#html_theme = "f5_sphinx_theme"
-html_theme = 'sphinx_rtd_theme'
-#html_theme_path = f5_sphinx_theme.get_html_theme_path()
+html_theme = "f5_sphinx_theme"
+#html_theme = 'sphinx_rtd_theme'
+html_theme_path = f5_sphinx_theme.get_html_theme_path()
 html_sidebars = {"**": ["searchbox.html", "localtoc.html", "globaltoc.html"]}
 html_theme_options = {
     "site_name": "Community Training Classes & Labs",
