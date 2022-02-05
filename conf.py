e4c43c06122e77c1ee3750994f005a39b891d20c
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-02-04 22:55:07 -0500

    creating skel

--- conf.py
+++ conf.py
@@ -189,7 +189,8 @@ html_theme_path = f5_sphinx_theme.get_html_theme_path()
 html_sidebars = {"**": ["searchbox.html", "localtoc.html", "globaltoc.html"]}
 html_theme_options = {
     "site_name": "Service Provider",
-    "next_prev_link": True
+    "next_prev_link": True,
+    'sidebar_span': 6
 }
 html_last_updated_fmt = "%Y-%m-%d %H:%M:%S"
 
