eff3c9209489f7eff7688c3d542fbfdc8f5685c8
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-02-18 11:35:21 -0500

    creating skel

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'bizstyle'
 
 html_sidebars = {'**': ['searchbox.html', 'localtoc.html', 'globaltoc.html']}
 
