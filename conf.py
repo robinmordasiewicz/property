1bd34e72bc1032bd8b9396a47b2b0082b14aa4de
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-02-18 12:22:32 -0500

    creating skel

--- conf.py
+++ conf.py
@@ -32,11 +32,11 @@ release = '202202152222'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
 #extensions = [
-#    "sphinxcontrib.youtube",
 #]
+extensions = [
+    "sphinxcontrib.youtube"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
