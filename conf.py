a78b0427ef536c574bbabefa79a0ae7787ea35e4
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-09 15:49:05 -0500

    creating skel

--- conf.py
+++ conf.py
@@ -63,6 +63,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #                        # 'base_url' = ''                            \\ DEFAULTS TO '/'
 #                     }
 
+
 html_theme = "f5_sphinx_theme"
 html_theme_path = f5_sphinx_theme.get_html_theme_path()
 html_sidebars = {"**": ["searchbox.html", "localtoc.html", "globaltoc.html"]}
@@ -77,3 +78,4 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_copy_source = False
